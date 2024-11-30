11c5fda4c4607ba65318d4b3257330f21e836d93
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-11-30 21:14:29 +0800

    add platform and edge collider and create a snowball to test gravity

--- Assets/Sprites/RollDown.cs
+++ Assets/Sprites/RollDown.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class RollDown : MonoBehaviour
 {
+    [SerializeField] float rollSpeed = 5;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // float rollDown = Input.GetAxis("Horizontal") * rollSpeed * Time.deltaTime;
+        // transform.Translate(0, rollDown, 0);
     }
 }
