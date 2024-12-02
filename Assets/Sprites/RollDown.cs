ddc278a912b0c0523ed06a32575f75c2096c8464
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-12-02 20:57:18 +0800

    add script roll down

--- Assets/Sprites/RollDown.cs
+++ Assets/Sprites/RollDown.cs
@@ -12,7 +12,7 @@ public class RollDown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // float rollDown = Input.GetAxis("Horizontal") * rollSpeed * Time.deltaTime;
-        // transform.Translate(0, rollDown, 0);
+        float rollDown = Input.GetAxis("Horizontal") * rollSpeed * Time.deltaTime;
+        transform.Translate(0, rollDown, 0);
     }
 }
