fa612cf918a7b7fefe8b6efc3d54c734fa557672
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-01-01 21:57:50 +0800

    Fixed bug going in the underworld

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,14 +3,24 @@ using UnityEngine;
 public class CrashDetector : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    CircleCollider2D playerHead;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        playerHead = GetComponent<CircleCollider2D>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            Debug.Log("You bumped");
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground" && playerHead.IsTouching(other.collider))
+        {
+            Debug.Log("Hit my Head!!");
+        }
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 public class FinishLine : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.tag == "Player")
+        {
+            Debug.Log("Finished");
+
+        }
     }
 }
